f13066f8aed1a6f85c7cb686402671e291f357ea
Sella-GH <147769367+Sella-GH@users.noreply.github.com>
2024-03-12 22:33:49 +0100

    Add basic start/stop playing commands

---
+++ AzzyBot/Modules/MusicStreaming/LavalinkService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Threading.Tasks;
+using AzzyBot.Modules.AzuraCast.Enums;
+using AzzyBot.Settings.AzuraCast;
+using AzzyBot.Settings.MusicStreaming;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+using Lavalink4NET.Players;
+using Lavalink4NET.Rest.Entities;
+using Lavalink4NET.Rest.Entities.Tracks;
+using Lavalink4NET.Tracks;
+using Microsoft.Extensions.Options;
+
+namespace AzzyBot.Modules.MusicStreaming;
+
+internal static class LavalinkService
+{
+    private static async ValueTask<LavalinkPlayer?> GetPlayerAsync(InteractionContext ctx, bool connectToVoice = true)
+    {
+        PlayerRetrieveOptions retrieveOptions = new((connectToVoice) ? PlayerChannelBehavior.Join : PlayerChannelBehavior.None);
+        LavalinkPlayerOptions playerOptions = new() { DisconnectOnDestroy = true, SelfDeaf = true };
+        PlayerResult<LavalinkPlayer> result = await Program.GetAudioService.Players.RetrieveAsync(ctx.Guild.Id, ctx.Member?.VoiceState.Channel.Id, PlayerFactory.Default, Options.Create(playerOptions), retrieveOptions);
+
+        if (!result.IsSuccess)
+        {
+            string errorMessage = result.Status switch
+            {
+                PlayerRetrieveStatus.BotNotConnected => "The bot is currently not connected",
+                PlayerRetrieveStatus.UserNotInVoiceChannel => "You are not connected to a voice channel",
+                _ => "An unknown error occurred"
+            };
+
+            DiscordFollowupMessageBuilder errorResponse = new DiscordFollowupMessageBuilder().WithContent(errorMessage).AsEphemeral();
+
+            await ctx.FollowUpAsync(errorResponse);
+
+            return null;
+        }
+
+        return result.Player;
+    }
+
+    internal static async Task PlayMusicAsync(InteractionContext ctx)
+    {
+        LavalinkPlayer? player = await GetPlayerAsync(ctx);
+
+        if (player is null)
+            return;
+
+        if (player.CurrentTrack is not null)
+        {
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("Player is busy"));
+            return;
+        }
+
+        TrackLoadOptions trackLoadOptions = new()
+        {
+            SearchMode = TrackSearchMode.None,
+            SearchBehavior = StrictSearchBehavior.Passthrough,
+            CacheMode = CacheMode.Bypass
+        };
+
+        string url = string.Join("/", AzuraCastSettings.AzuraApiUrl.Replace("/api", string.Empty, StringComparison.OrdinalIgnoreCase), AzuraCastApiEnum.listen, MusicStreamingSettings.MountPointStub);
+        LavalinkTrack? track = await Program.GetAudioService.Tracks.LoadTrackAsync(url, trackLoadOptions);
+
+        if (track is null)
+            return;
+
+        await player.PlayAsync(track);
+    }
+
+    internal static async Task StopMusicAsync(InteractionContext ctx)
+    {
+        LavalinkPlayer? player = await GetPlayerAsync(ctx, false);
+
+        if (player is null)
+            return;
+
+        if (player.CurrentTrack is null)
+        {
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("Nothing playing"));
+            return;
+        }
+
+        await player.StopAsync();
+        await player.DisconnectAsync();
+        await player.DisposeAsync();
+    }
+}
--- AzzyBot/Modules/MusicStreaming/MusicStreamingCommands.cs
+++ AzzyBot/Modules/MusicStreaming/MusicStreamingCommands.cs
@@ -1,8 +1,53 @@
-﻿using DSharpPlus.SlashCommands;
+﻿using System.Threading.Tasks;
+using AzzyBot.ExceptionHandling;
+using AzzyBot.Modules.AzuraCast;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+using DSharpPlus.SlashCommands.Attributes;
+using Microsoft.Extensions.Logging;
 
 namespace AzzyBot.Modules.MusicStreaming;
 
 internal sealed class MusicStreamingCommands : ApplicationCommandModule
 {
+    [SlashCommandGroup("player", "Player commands")]
+    [SlashRequireGuild]
+    internal sealed class PlayerCommandGroup : ApplicationCommandModule
+    {
+        [SlashCommand("start", "Starts the music stream into your voice channel")]
+        internal static async Task PlayerStartCommandAsync(InteractionContext ctx)
+        {
+            ExceptionHandler.LogMessage(LogLevel.Debug, "PlayerStartCommandAsync requested");
 
+            await ctx.CreateResponseAsync(DSharpPlus.InteractionResponseType.DeferredChannelMessageWithSource);
+
+            if (!await AzuraCastModule.CheckIfMusicServerIsOnlineAsync())
+            {
+                await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(AzuraCastEmbedBuilder.BuildServerIsOfflineEmbed(ctx.Client.CurrentUser.Username, ctx.Client.CurrentUser.AvatarUrl, false)));
+                return;
+            }
+
+            await LavalinkService.PlayMusicAsync(ctx);
+
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("Track's playing"));
+        }
+
+        [SlashCommand("stop", "Stops the music stream from playing")]
+        internal static async Task PlayerStopCommandAsync(InteractionContext ctx)
+        {
+            ExceptionHandler.LogMessage(LogLevel.Debug, "PlayerStopCommandAsync requested");
+
+            await ctx.CreateResponseAsync(DSharpPlus.InteractionResponseType.DeferredChannelMessageWithSource);
+
+            if (!await AzuraCastModule.CheckIfMusicServerIsOnlineAsync())
+            {
+                await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(AzuraCastEmbedBuilder.BuildServerIsOfflineEmbed(ctx.Client.CurrentUser.Username, ctx.Client.CurrentUser.AvatarUrl, false)));
+                return;
+            }
+
+            await LavalinkService.StopMusicAsync(ctx);
+
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("Track's stopping"));
+        }
+    }
 }
