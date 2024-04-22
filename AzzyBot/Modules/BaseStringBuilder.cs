703d38936f3fc11e50a25665727268ebe2533129
Sella-GH <147769367+Sella-GH@users.noreply.github.com>
2024-04-22 21:56:36 +0200

    Add new check if strings are missing

--- AzzyBot/Modules/BaseStringBuilder.cs
+++ AzzyBot/Modules/BaseStringBuilder.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
+using System.Reflection;
 using System.Threading.Tasks;
+using AzzyBot.Logging;
 using AzzyBot.Modules;
 using AzzyBot.Modules.AzuraCast.Strings;
 using AzzyBot.Modules.ClubManagement.Strings;
+using AzzyBot.Modules.Core;
 using AzzyBot.Modules.Core.Strings;
 using AzzyBot.Modules.MusicStreaming.Strings;
 
@@ -11,19 +16,69 @@ namespace AzzyBot.Strings;
 
 internal abstract class BaseStringBuilder
 {
+    [SuppressMessage("Roslynator", "RCS1208:Reduce 'if' nesting", Justification = "Code Style")]
     internal static async Task LoadStringsAsync()
     {
         if (!await CoreStringBuilder.LoadCoreStringsAsync())
-            throw new InvalidOperationException("Core strings can't be loaded");
+        {
+            LoggerBase.LogError(LoggerBase.GetLogger, "Core strings can't be loaded", null);
+            await AzzyBot.BotShutdownAsync();
+        }
 
         if (ModuleStates.AzuraCast && !await AcStringBuilder.LoadAzuraCastStringsAsync())
-            throw new InvalidOperationException("AzuraCast strings can't be loaded");
+        {
+            LoggerBase.LogError(LoggerBase.GetLogger, "AzuraCast strings can't be loaded", null);
+            await AzzyBot.BotShutdownAsync();
+        }
 
         if (ModuleStates.ClubManagement && !await CmStringBuilder.LoadClubManagementStringsAsync())
-            throw new InvalidOperationException("ClubManagement strings can't be loaded");
+        {
+            LoggerBase.LogError(LoggerBase.GetLogger, "ClubManagement strings can't be loaded", null);
+            await AzzyBot.BotShutdownAsync();
+        }
 
         if (ModuleStates.MusicStreaming && !await MsStringBuilder.LoadMusicStreamingStringsAsync())
-            throw new InvalidOperationException("MusicStreaming strings can't be loaded");
+        {
+            LoggerBase.LogError(LoggerBase.GetLogger, "MusicStreaming strings can't be loaded", null);
+            await AzzyBot.BotShutdownAsync();
+        }
+    }
+
+    protected static bool CheckStrings(Type type)
+    {
+        // Get all Properties of the class
+        PropertyInfo[] properties = type.GetProperties(BindingFlags.NonPublic | BindingFlags.Static);
+
+        List<string> failed = [];
+
+        // Loop through all properties and check if they are null, whitespace or 0
+        // If yes add them to the list
+        foreach (PropertyInfo property in properties)
+        {
+            if (property.PropertyType != typeof(string))
+                continue;
+
+            string? value = property.GetValue(null) as string;
+
+            if (string.IsNullOrWhiteSpace(value))
+                failed.Add(property.Name);
+        }
+
+        if (failed.Count == 0)
+            return true;
+
+        foreach (string item in failed)
+        {
+            LoggerBase.LogError(LoggerBase.GetLogger, $"String {item} has to be filled out!", null);
+        }
+
+        if (CoreMisc.CheckIfLinuxOs())
+            return false;
+
+        LoggerBase.LogInfo(LoggerBase.GetLogger, "Press any key to acknowledge...", null);
+        Console.ReadKey();
+
+        return false;
     }
 
     protected static string BuildString(string template, string parameterName, double parameterValue) => template.Replace(parameterName, parameterValue.ToString(CultureInfo.InvariantCulture), StringComparison.InvariantCultureIgnoreCase);
