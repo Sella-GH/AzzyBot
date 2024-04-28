c10470cccf9fae4348c64763b8aaffd1ce18aa26
Sella-GH <147769367+Sella-GH@users.noreply.github.com>
2024-04-28 23:21:31 +0200

    Remove useless command groups

--- AzzyBot/Modules/Core/CoreCommands.cs
+++ AzzyBot/Modules/Core/CoreCommands.cs
@@ -37,28 +37,20 @@ internal sealed class CoreCommands : ApplicationCommandModule
     [RequireUserRole]
     internal sealed class CoreCommandGroup : ApplicationCommandModule
     {
-        [SlashCommandGroup("info", "Info commands")]
-        internal sealed class CoreInfoCommandGroup : ApplicationCommandModule
+        [SlashCommand("info", "Shows basic information about Azzy")]
+        internal static async Task CoreInfoCommandAsync(InteractionContext ctx)
         {
-            [SlashCommand("azzy", "Shows basic information about Azzy")]
-            internal static async Task CoreInfoAzzyCommandAsync(InteractionContext ctx)
-            {
-                LoggerBase.LogInfo(LoggerBase.GetLogger, "CoreInfoAzzyCommand requsted", null);
-                await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
-                await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(await CoreEmbedBuilder.BuildInfoAzzyEmbedAsync(ctx.Client.CurrentUser.Username, ctx.Client.CurrentUser.AvatarUrl)));
-            }
+            LoggerBase.LogInfo(LoggerBase.GetLogger, "CoreInfoCommand requsted", null);
+            await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(await CoreEmbedBuilder.BuildInfoAzzyEmbedAsync(ctx.Client.CurrentUser.Username, ctx.Client.CurrentUser.AvatarUrl)));
         }
 
-        [SlashCommandGroup("ping", "Ping commands")]
-        internal sealed class CorePingCommandGroup : ApplicationCommandModule
+        [SlashCommand("ping", "Pings Azzy and returns general information")]
+        internal static async Task CorePingCommandAsync(InteractionContext ctx)
         {
-            [SlashCommand("azzy", "Pings Azzy and returns general information")]
-            internal static async Task CorePingAzzyCommandAsync(InteractionContext ctx)
-            {
-                LoggerBase.LogInfo(LoggerBase.GetLogger, "CorePingAzzyCommand requested", null);
-                await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
-                await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(await CoreEmbedBuilder.BuildAzzyStatsEmbedAsync(ctx.Client.CurrentUser.Username, ctx.Client.CurrentUser.AvatarUrl, ctx.Client.Ping)));
-            }
+            LoggerBase.LogInfo(LoggerBase.GetLogger, "CorePingCommand requested", null);
+            await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(await CoreEmbedBuilder.BuildAzzyStatsEmbedAsync(ctx.Client.CurrentUser.Username, ctx.Client.CurrentUser.AvatarUrl, ctx.Client.Ping)));
         }
     }
 }
