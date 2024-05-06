a092ac003e56d671fa5fb87ec0be964973f8b6c6
Sella-GH <147769367+Sella-GH@users.noreply.github.com>
2024-05-06 19:44:33 +0200

    Add FileOperations with TempFile stuff back

---
+++ AzzyBot-Next/Utilities/FileOperations.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using System;
+
+namespace AzzyBot.Utilities;
+
+internal static class FileOperations
+{
+    internal static async Task<string> CreateTempFileAsync(string content, string? fileName = null)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(content, nameof(content));
+
+        string tempFilePath;
+
+        if (!string.IsNullOrWhiteSpace(fileName))
+        {
+            tempFilePath = Path.Combine(Path.GetTempPath(), fileName);
+        }
+        else
+        {
+            tempFilePath = Path.GetTempFileName();
+        }
+
+        await File.WriteAllTextAsync(tempFilePath, content);
+
+        return tempFilePath;
+    }
+
+    internal static void DeleteTempFilePath(string path)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(path, nameof(path));
+
+        File.Delete(path);
+    }
+}
