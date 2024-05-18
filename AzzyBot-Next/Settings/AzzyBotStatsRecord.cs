0e2066d32b3e5ff21cd004ad2c86c21fed47cf28
Sella-GH <147769367+Sella-GH@users.noreply.github.com>
2024-05-18 22:10:40 +0200

    Adjust actions, dockerfile and workflow to new name scheme

--- AzzyBot-Next/Settings/AzzyBotStatsRecord.cs
+++ AzzyBot-Next/Settings/AzzyBotStatsRecord.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.Text.Json.Serialization;
 
 namespace AzzyBot.Settings;
 
 internal sealed record AzzyBotStatsRecord
 {
+    [JsonPropertyName(nameof(Commit))]
     public required string Commit { get; init; }
+
+    [JsonPropertyName(nameof(CompilationDate))]
     public required DateTime CompilationDate { get; init; }
+
+    [JsonPropertyName(nameof(LocCs))]
     public required int LocCs { get; init; }
-    public required int LocJson { get; init; }
 }
