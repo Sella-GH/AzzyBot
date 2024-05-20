ce937e90d5db40dfe5f4fb3ec734e1a76878971f
Sella-GH <147769367+Sella-GH@users.noreply.github.com>
2024-05-20 21:39:10 +0200

    Use primary constructor

--- AzzyBot-Next/Utilities/Encryption/AesGcmCipher.cs
+++ AzzyBot-Next/Utilities/Encryption/AesGcmCipher.cs
@@ -4,13 +4,13 @@ using System.Security.Cryptography;
 
 namespace AzzyBot.Utilities.Encryption;
 
-public sealed class AesGcmCipher
+internal sealed class AesGcmCipher(byte[] nonce, byte[] tag, byte[] cipher)
 {
-    public byte[] Nonce { get; }
-    public byte[] Tag { get; }
-    public byte[] Cipher { get; }
+    internal byte[] Nonce { get; } = nonce;
+    internal byte[] Tag { get; } = tag;
+    internal byte[] Cipher { get; } = cipher;
 
-    public static AesGcmCipher FromBase64String(string data)
+    internal static AesGcmCipher FromBase64String(string data)
     {
         byte[] dataBytes = Convert.FromBase64String(data);
         return new(
@@ -18,13 +18,6 @@ public sealed class AesGcmCipher
             dataBytes[^AesGcm.TagByteSizes.MaxSize..],
             dataBytes[AesGcm.NonceByteSizes.MaxSize..^AesGcm.TagByteSizes.MaxSize]
         );
-    }
-
-    public AesGcmCipher(byte[] nonce, byte[] tag, byte[] cipher)
-    {
-        Nonce = nonce;
-        Tag = tag;
-        Cipher = cipher;
     }
 
     public override string ToString() => Convert.ToBase64String(Nonce.Concat(Cipher).Concat(Tag).ToArray());
