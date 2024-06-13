141d63ff48a59d10d818a7a0bf8669437972d861
Sella-GH <147769367+Sella-GH@users.noreply.github.com>
2024-06-13 21:37:57 +0200

    Improve Help Command performance

--- AzzyBot-Next/Utilities/AzzyHelp.cs
+++ AzzyBot-Next/Utilities/AzzyHelp.cs
@@ -39,47 +39,58 @@ public static class AzzyHelp
         ArgumentOutOfRangeException.ThrowIfZero(commands.Count, nameof(commands));
         ArgumentException.ThrowIfNullOrWhiteSpace(commandName, nameof(commandName));
 
-        return GetCommandGroups(commands, adminServer, approvedDebug, member).FirstOrDefault(r => r.Value.Any(c => c.Name == commandName)).Value.FirstOrDefault() ?? throw new InvalidOperationException("Command not found");
+        return GetCommandGroups(commands, adminServer, approvedDebug, member, true).FirstOrDefault(r => r.Value.Any(c => c.Name == commandName)).Value.FirstOrDefault() ?? throw new InvalidOperationException("Command not found");
     }
 
-    private static Dictionary<string, List<AzzyHelpRecord>> GetCommandGroups(IReadOnlyDictionary<string, Command> commands, bool adminServer, bool approvedDebug, DiscordMember member)
+    private static Dictionary<string, List<AzzyHelpRecord>> GetCommandGroups(IReadOnlyDictionary<string, Command> commands, bool adminServer, bool approvedDebug, DiscordMember member, bool singleCommand = false)
     {
         List<string> commandGroups = commands.Where(c => c.Value.Subcommands.Count > 0 && CheckIfMemberHasPermission(adminServer, approvedDebug, member, c.Value.Name)).Select(c => c.Value.Name).ToList();
         Dictionary<string, List<AzzyHelpRecord>> records = [];
         foreach (string group in commandGroups)
         {
-            records.Add(group, GetCommands(commands[group].Subcommands, group));
+            records.Add(group, GetCommands(commands[group].Subcommands, group, singleCommand));
         }
 
         return records;
     }
 
-    private static List<AzzyHelpRecord> GetCommands(IReadOnlyList<Command> commands, string subCommand = "")
+    private static List<AzzyHelpRecord> GetCommands(IReadOnlyList<Command> commands, string subCommand = "", bool singleCommand = false)
     {
         List<AzzyHelpRecord> records = [];
         foreach (Command command in commands)
         {
             string description = command.Description ?? "No description provided.";
             Dictionary<string, string> parameters = [];
-            foreach (CommandParameter parameter in command.Parameters)
-            {
-                string paramName = parameter.Name ?? "No name provided";
-                string paramDescription = parameter.Description ?? "No description provided.";
-                if (parameter.DefaultValue.HasValue)
-                {
-                    paramName += " (optional)";
-                }
-                else
-                {
-                    paramName += " (required)";
-                }
-
-                parameters.Add(paramName, paramDescription);
-            }
+            if (singleCommand)
+                parameters = GetCommandParameters(command);
 
             records.Add(new AzzyHelpRecord(subCommand, command.FullName, description, parameters));
         }
 
         return records;
+    }
+
+    private static Dictionary<string, string> GetCommandParameters(Command command)
+    {
+        ArgumentNullException.ThrowIfNull(command, nameof(command));
+
+        Dictionary<string, string> parameters = [];
+        foreach (CommandParameter parameter in command.Parameters)
+        {
+            string paramName = parameter.Name ?? "No name provided";
+            string paramDescription = parameter.Description ?? "No description provided.";
+            if (parameter.DefaultValue.HasValue)
+            {
+                paramName += " (optional)";
+            }
+            else
+            {
+                paramName += " (required)";
+            }
+
+            parameters.Add(paramName, paramDescription);
+        }
+
+        return parameters;
     }
 }
