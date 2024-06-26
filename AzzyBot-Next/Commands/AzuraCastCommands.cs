aafe8dcb8b6e8c792236aa90d7528db49bbb8f3b
Sellara <147769367+Sella-GH@users.noreply.github.com>
2024-06-26 19:16:39 +0200

    Add debug logging

--- AzzyBot-Next/Commands/AzuraCastCommands.cs
+++ AzzyBot-Next/Commands/AzuraCastCommands.cs
@@ -403,6 +403,11 @@ public sealed class AzuraCastCommands
             isQueued = queue.Any(q => q.Song.SongId == songRequest.Song.SongId || q.Song.UniqueId == songRequest.Song.UniqueId);
             isRequested = requestsPending.Any(r => r.Track.SongId == songRequest.Song.SongId || r.Track.UniqueId == songRequest.Song.UniqueId);
 
+            _logger.LogDebug(stationConfig.EnableRequests.ToString());
+            _logger.LogDebug(isQueued.ToString());
+            _logger.LogDebug(isRequested.ToString());
+            _logger.LogDebug(isPlayed.ToString());
+
             DiscordEmbed embed = EmbedBuilder.BuildAzuraCastMusicSearchSongEmbed(songRequest);
             if (!stationConfig.EnableRequests || !isQueued || !isRequested || !isPlayed)
             {
