2d20dfb771815d2a679fc496f31c5f7570284c65
Sellara <147769367+Sella-GH@users.noreply.github.com>
2024-07-06 22:03:49 +0200

    Fix default port

--- src/AzzyBot-Next/Settings/AzzyBotSettingsRecord.cs
+++ src/AzzyBot-Next/Settings/AzzyBotSettingsRecord.cs
@@ -20,7 +20,7 @@ public sealed record DatabaseSettings
 {
     public required string EncryptionKey { get; init; } // 32 Characters
     public string Host { get; init; } = string.Empty;
-    public int Port { get; init; } = 3306;
+    public int Port { get; init; }
     public string User { get; init; } = string.Empty;
     public string Password { get; init; } = string.Empty;
     public string DatabaseName { get; init; } = string.Empty;
