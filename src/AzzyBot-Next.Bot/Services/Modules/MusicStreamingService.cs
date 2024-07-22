df36f30ef440877799d9cf6ca51df7a6daed25c2
Sella-GH <147769367+Sella-GH@users.noreply.github.com>
2024-07-22 22:08:18 +0200

    Try a fix for null ref

--- src/AzzyBot-Next.Bot/Services/Modules/MusicStreamingService.cs
+++ src/AzzyBot-Next.Bot/Services/Modules/MusicStreamingService.cs
@@ -22,8 +22,9 @@ public sealed class MusicStreamingService(IAudioService audioService)
 
         LavalinkPlayerOptions playerOptions = new() { SelfDeaf = true };
         PlayerRetrieveOptions retrieveOptions = new((connectToVoice) ? PlayerChannelBehavior.Join : PlayerChannelBehavior.None, MemberVoiceStateBehavior.RequireSame);
+        ulong channelId = context.Member?.VoiceState.Channel?.Id ?? 0;
 
-        PlayerResult<LavalinkPlayer> player = await _audioService.Players.RetrieveAsync(context.Guild.Id, context.Member?.VoiceState.Channel?.Id ?? 0, PlayerFactory.Default, Options.Create(playerOptions), retrieveOptions);
+        PlayerResult<LavalinkPlayer> player = await _audioService.Players.RetrieveAsync(context.Guild.Id, channelId, PlayerFactory.Default, Options.Create(playerOptions), retrieveOptions);
         if (player.IsSuccess)
             return player.Player;
 
