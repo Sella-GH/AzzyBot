5655609dee2a683eff27ed5b0611fa7d1f1fc20b
Sella-GH <147769367+Sella-GH@users.noreply.github.com>
2024-07-25 00:16:36 +0200

    Add fix for station being offline

--- src/AzzyBot-Next.Bot/Commands/MusicStreamingCommands.cs
+++ src/AzzyBot-Next.Bot/Commands/MusicStreamingCommands.cs
@@ -2,12 +2,17 @@
 using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using AzzyBot.Bot.Commands.Autocompletes;
 using AzzyBot.Bot.Commands.Checks;
 using AzzyBot.Bot.Services.Modules;
 using AzzyBot.Bot.Utilities.Enums;
+using AzzyBot.Bot.Utilities.Records.AzuraCast;
 using AzzyBot.Core.Logging;
+using AzzyBot.Core.Utilities.Encryption;
+using AzzyBot.Data;
+using AzzyBot.Data.Entities;
 using DSharpPlus.Commands;
 using DSharpPlus.Commands.ContextChecks;
 using DSharpPlus.Commands.Processors.SlashCommands.ArgumentModifiers;
@@ -20,9 +25,11 @@ namespace AzzyBot.Bot.Commands;
 public sealed class MusicStreamingCommands
 {
     [Command("player"), RequireGuild, RequirePermissions(DiscordPermissions.Speak | DiscordPermissions.UseVoice, DiscordPermissions.UseVoice)]
-    public sealed class PlayerGroup(ILogger<PlayerGroup> logger, MusicStreamingService musicStreaming)
+    public sealed class PlayerGroup(ILogger<PlayerGroup> logger, AzuraCastApiService azuraCast, DbActions dbActions, MusicStreamingService musicStreaming)
     {
         private readonly ILogger<PlayerGroup> _logger = logger;
+        private readonly AzuraCastApiService _azuraCast = azuraCast;
+        private readonly DbActions _dbActions = dbActions;
         private readonly MusicStreamingService _musicStreaming = musicStreaming;
 
         [Command("join"), Description("Join the voice channel.")]
@@ -64,7 +71,6 @@ public sealed class MusicStreamingCommands
             await context.EditResponseAsync("I'm gone now.");
         }
 
-        [SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Needed for the AutoComplete.")]
         [Command("play"), Description("Choose a mount point of the station."), ModuleActivatedCheck(AzzyModules.AzuraCast), AzuraCastOnlineCheck]
         public async Task PlayAsync
         (
@@ -74,8 +80,21 @@ public sealed class MusicStreamingCommands
         )
         {
             ArgumentNullException.ThrowIfNull(context, nameof(context));
+            ArgumentNullException.ThrowIfNull(context.Guild, nameof(context.Guild));
 
             _logger.CommandRequested(nameof(PlayAsync), context.User.GlobalName);
+
+            AzuraCastEntity azura = await _dbActions.GetAzuraCastAsync(context.Guild.Id) ?? throw new InvalidOperationException("AzuraCast is not set up for this server.");
+            AzuraCastStationEntity stationEntity = azura.Stations.FirstOrDefault(x => x.StationId == station) ?? throw new InvalidOperationException("Station not found.");
+            try
+            {
+                AzuraNowPlayingDataRecord nowPlaying = await _azuraCast.GetNowPlayingAsync(new(Crypto.Decrypt(azura.BaseUrl)), station);
+            }
+            catch (HttpRequestException)
+            {
+                await context.EditResponseAsync("This station is currently offline.");
+                return;
+            }
 
             await _musicStreaming.PlayMusicAsync(context, mountPoint);
 
