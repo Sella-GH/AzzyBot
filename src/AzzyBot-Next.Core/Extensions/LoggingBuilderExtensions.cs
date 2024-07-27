06fe4520a485a0f8f4fe668bedce396942bb5f6b
Sellara <147769367+Sella-GH@users.noreply.github.com>
2024-07-27 17:50:23 +0200

    Update some logging filters

--- src/AzzyBot-Next.Core/Extensions/LoggingBuilderExtensions.cs
+++ src/AzzyBot-Next.Core/Extensions/LoggingBuilderExtensions.cs
@@ -41,13 +41,19 @@ public static class LoggingBuilderExtensions
             Directory.CreateDirectory("Logs");
 
         logging.AddFile(config => config.Directory = "Logs");
+        logging.AddFilter("Microsoft.EntityFrameworkCore.ChangeTracking", LogLevel.Warning);
         logging.AddFilter("Microsoft.EntityFrameworkCore.Database", (isDev || forceDebug) ? LogLevel.Debug : LogLevel.Warning);
+        logging.AddFilter("Microsoft.EntityFrameworkCore.Database.Connection", LogLevel.Warning);
         logging.AddFilter("Microsoft.EntityFrameworkCore.Database.Command", (isDev || forceDebug) ? LogLevel.Debug : LogLevel.Warning);
+        logging.AddFilter("Microsoft.EntityFrameworkCore.Database.Transaction", LogLevel.Warning);
         logging.AddFilter("Microsoft.EntityFrameworkCore.Infrastructure", (isDev || forceDebug) ? LogLevel.Debug : LogLevel.Warning);
         logging.AddFilter("Microsoft.EntityFrameworkCore.Migrations", (isDev || forceDebug) ? LogLevel.Debug : LogLevel.Information);
+        logging.AddFilter("Microsoft.EntityFrameworkCore.Query", LogLevel.Warning);
         logging.AddFilter("Microsoft.Extensions.Hosting", LogLevel.Warning);
         logging.AddFilter("Microsoft.Extensions.Http.DefaultHttpClientFactory", LogLevel.Warning);
         logging.AddFilter("Microsoft.Hosting.Lifetime", LogLevel.Warning);
+        logging.AddFilter("System.Net.Http.HttpClient.Default.ClientHandler", LogLevel.Warning);
+        logging.AddFilter("System.Net.Http.HttpClient.Default.LogicalHandler", LogLevel.Warning);
         logging.AddSimpleConsole(config =>
         {
             config.ColorBehavior = LoggerColorBehavior.Enabled;
