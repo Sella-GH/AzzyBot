32a78cdf4bba0fef4b9aac86b7fe68c22ce63ab4
Sella-GH <147769367+Sella-GH@users.noreply.github.com>
2024-07-28 19:35:33 +0200

    Add new IEnumerable Extension class

---
+++ src/AzzyBot-Next.Core/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AzzyBot.Core.Extensions;
+
+public static class IEnumerableExtensions
+{
+    public static bool ContainsOneItem<T>(this IEnumerable<T> enumerable)
+    {
+        ArgumentNullException.ThrowIfNull(enumerable, nameof(enumerable));
+
+        using IEnumerator<T> enumerator = enumerable.GetEnumerator();
+        return enumerator.MoveNext() && !enumerator.MoveNext();
+    }
+}
