d001ca96cd2b24ee2f5eda11c4ddfedb5cd3d7af
Sella-GH <147769367+Sella-GH@users.noreply.github.com>
2024-08-01 15:55:11 +0200

    Add new record and file checker

---
+++ src/AzzyBot.Bot/Utilities/Records/AzuraCast/AzuraFileComplianceRecord.cs
@@ -0,0 +1,15 @@
+﻿namespace AzzyBot.Bot.Utilities.Records.AzuraCast;
+
+public sealed record AzuraFileComplianceRecord
+{
+    public bool IsCompliant { get; init; }
+    public bool TitleCompliance { get; init; }
+    public bool PerformerCompliance { get; init; }
+
+    public AzuraFileComplianceRecord(bool isCompliant, bool titleCompliance, bool performerCompliance)
+    {
+        IsCompliant = isCompliant;
+        TitleCompliance = titleCompliance;
+        PerformerCompliance = performerCompliance;
+    }
+}
