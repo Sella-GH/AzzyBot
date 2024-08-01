1da2c5b0124a32fafaea05a6bba23c59d5fc94d2
Sella-GH <147769367+Sella-GH@users.noreply.github.com>
2024-08-01 19:06:13 +0200

    Change some basic things

--- src/AzzyBot.Web/Startup.cs
+++ src/AzzyBot.Web/Startup.cs
@@ -4,7 +4,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace AzzyBot.Web;
-public static class Program
+
+public static class Startup
 {
     public static void Main(string[] args)
     {
