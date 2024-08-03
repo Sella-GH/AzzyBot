da6f0a7222e9b2e6d394b7aa2d950e0a294b6271
Sella-GH <147769367+Sella-GH@users.noreply.github.com>
2024-08-03 21:57:41 +0200

    Use AnyCPU by default

--- src/AzzyBot.Data/Extensions/IServiceCollectionExtensions.cs
+++ src/AzzyBot.Data/Extensions/IServiceCollectionExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 using System.Text;
 using AzzyBot.Core.Utilities;
 using AzzyBot.Core.Utilities.Encryption;
