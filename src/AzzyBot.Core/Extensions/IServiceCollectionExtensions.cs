b81b138b2ceef40094e2781a0defed5fb93ff0cc
Sella-GH <147769367+Sella-GH@users.noreply.github.com>
2024-08-03 21:12:24 +0200

    Move some more stuff around

---
+++ src/AzzyBot.Core/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using AzzyBot.Core.Utilities;
+using AzzyBot.Core.Utilities.Records;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AzzyBot.Core.Extensions;
+
+public static class IServiceCollectionExtensions
+{
+    public static void AzzyBotStats(this IServiceCollection services, bool isDev)
+    {
+        if (isDev)
+        {
+            services.AddSingleton(new AppStatsRecord("Unknown", DateTime.Now, 0));
+            return;
+        }
+
+        string path = Path.Combine("Modules", "Core", "Files", "AppStats.json");
+
+        AppStatsRecord? stats = Misc.GetConfiguration(path).Get<AppStatsRecord>();
+        if (stats is null)
+        {
+            Console.Error.Write("There is something wrong with your configuration. Did you followed the installation instructions?");
+            if (!HardwareStats.CheckIfLinuxOs)
+                Console.ReadKey();
+
+            Environment.Exit(1);
+        }
+
+        services.AddSingleton(stats);
+    }
+}
---
+++ src/AzzyBot.Data/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using AzzyBot.Core.Utilities;
+using AzzyBot.Core.Utilities.Encryption;
+using EntityFramework.Exceptions.PostgreSQL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+
+namespace AzzyBot.Data.Extensions;
+
+public static class IServiceCollectionExtensions
+{
+    public static void AzzyBotDataServices(this IServiceCollection services, bool isDev, string encryptionKey, string? host, int? port, string? user, string? password, string? database)
+    {
+        // Set the encryption key
+        Crypto.EncryptionKey = Encoding.UTF8.GetBytes(encryptionKey);
+
+        string connectionString = GetConnectionString(isDev, host, port, user, password, database);
+        services.AddDbContextPool<AzzyDbContext>(o => o.UseNpgsql(connectionString).UseExceptionProcessor().EnableSensitiveDataLogging(isDev));
+        services.AddSingleton<DbActions>();
+    }
+
+    [SuppressMessage("Style", "IDE0045:Convert to conditional expression", Justification = "No nested conditional expressions")]
+    private static string GetConnectionString(bool isDev, string? host, int? port, string? user, string? password, string? database)
+    {
+        NpgsqlConnectionStringBuilder builder = [];
+        builder.Host = (string.IsNullOrWhiteSpace(host)) ? "AzzyBot-Db" : host;
+        builder.Port = port ?? 5432;
+        builder.Username = (string.IsNullOrWhiteSpace(user)) ? "azzybot" : user;
+        builder.ConnectionIdleLifetime = 1200; // 20 minutes
+
+        // No password because it can be null when using non-docker
+        builder.Password = (string.IsNullOrWhiteSpace(password) && HardwareStats.CheckIfDocker) ? "thisIsAzzyB0!P@ssw0rd" : password;
+        if (string.IsNullOrWhiteSpace(database))
+        {
+            builder.Database = (isDev) ? "azzybot-dev" : "azzybot";
+        }
+        else
+        {
+            builder.Database = database;
+        }
+
+        return builder.ConnectionString;
+    }
+}
