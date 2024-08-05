83015db933d11c96ae9ac4c0eaac51238345d829
Sella-GH <147769367+Sella-GH@users.noreply.github.com>
2024-08-05 03:46:50 +0200

    Add new commands

--- src/AzzyBot.Bot/Commands/MusicStreamingCommands.cs
+++ src/AzzyBot.Bot/Commands/MusicStreamingCommands.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
@@ -8,6 +9,7 @@ using AzzyBot.Bot.Commands.Autocompletes;
 using AzzyBot.Bot.Commands.Checks;
 using AzzyBot.Bot.Commands.Choices;
 using AzzyBot.Bot.Services.Modules;
+using AzzyBot.Bot.Utilities;
 using AzzyBot.Bot.Utilities.Enums;
 using AzzyBot.Bot.Utilities.Helpers;
 using AzzyBot.Bot.Utilities.Records.AzuraCast;
@@ -20,6 +22,8 @@ using DSharpPlus.Commands.ContextChecks;
 using DSharpPlus.Commands.Processors.SlashCommands;
 using DSharpPlus.Commands.Processors.SlashCommands.ArgumentModifiers;
 using DSharpPlus.Entities;
+using Lavalink4NET.Players;
+using Lavalink4NET.Tracks;
 using Microsoft.Extensions.Logging;
 
 namespace AzzyBot.Bot.Commands;
@@ -59,6 +63,26 @@ public sealed class MusicStreamingCommands
             await context.EditResponseAsync($"I set the volume to {volume}%.");
         }
 
+        [Command("history"), Description("Show the already played song history for this server.")]
+        public async ValueTask HistoryAsync(SlashCommandContext context)
+        {
+            ArgumentNullException.ThrowIfNull(context, nameof(context));
+
+            _logger.CommandRequested(nameof(HistoryAsync), context.User.GlobalName);
+
+            await context.DeferResponseAsync();
+
+            IEnumerable<ITrackQueueItem>? history = await _musicStreaming.HistoryAsync(context);
+            if (history?.Any() is not true)
+            {
+                await context.EditResponseAsync("There is no song history.");
+                return;
+            }
+
+            DiscordEmbed embed = EmbedBuilder.BuildMusicStreamingHistoryEmbed(history);
+            await context.EditResponseAsync(embed);
+        }
+
         [Command("join"), Description("Join the voice channel.")]
         public async ValueTask JoinAsync(SlashCommandContext context)
         {
@@ -96,6 +120,69 @@ public sealed class MusicStreamingCommands
                 return;
 
             await context.EditResponseAsync(GeneralStrings.VoiceLeft);
+        }
+
+        [Command("now-playing"), Description("Show the song which is playing right now.")]
+        public async ValueTask NowPlayingAsync(SlashCommandContext context)
+        {
+            ArgumentNullException.ThrowIfNull(context, nameof(context));
+
+            _logger.CommandRequested(nameof(NowPlayingAsync), context.User.GlobalName);
+
+            await context.DeferResponseAsync();
+
+            LavalinkTrack? track = await _musicStreaming.NowPlayingAsync(context);
+            TimeSpan? pos = await _musicStreaming.GetCurrentPositionAsync(context);
+            if (track is null || pos is null)
+            {
+                await context.EditResponseAsync("Nothing is playing right now.");
+                return;
+            }
+
+            DiscordEmbed embed = EmbedBuilder.BuildMusicStreamingNowPlayingEmbed(track, pos);
+
+            await context.EditResponseAsync(embed);
+        }
+
+        [Command("pause"), Description("Pause the current played song.")]
+        public async ValueTask PauseAsync(SlashCommandContext context)
+        {
+            ArgumentNullException.ThrowIfNull(context, nameof(context));
+
+            _logger.CommandRequested(nameof(PauseAsync), context.User.GlobalName);
+
+            await context.DeferResponseAsync();
+
+            if (await _musicStreaming.NowPlayingAsync(context) is null)
+            {
+                await context.EditResponseAsync("Nothing is playing right now.");
+                return;
+            }
+
+            await _musicStreaming.PauseAsync(context);
+
+            await context.EditResponseAsync("The music has been paused.");
+        }
+
+        [Command("play"), Description("Choose a provider and play the track via the url.")]
+        public async ValueTask PlayAsync
+        (
+            SlashCommandContext context,
+            [Description("The provider you want to search for."), SlashChoiceProvider<MusicStreamingPlatformProvider>] string provider,
+            [Description("The url of the track you want to play.")] string track
+        )
+        {
+            ArgumentNullException.ThrowIfNull(context, nameof(context));
+
+            _logger.CommandRequested(nameof(PlayAsync), context.User.GlobalName);
+
+            await context.DeferResponseAsync();
+
+            string? text = await _musicStreaming.PlayMusicAsync(context, track, new(provider));
+            if (string.IsNullOrWhiteSpace(text))
+                return;
+
+            await context.EditResponseAsync(text);
         }
 
         [Command("play-mount"), Description("Choose a mount point of the station to play it."), ModuleActivatedCheck(AzzyModules.AzuraCast), AzuraCastOnlineCheck]
@@ -142,7 +229,87 @@ public sealed class MusicStreamingCommands
             await context.EditResponseAsync(GeneralStrings.VoicePlayMount.Replace("%station%", nowPlaying.Station.Name, StringComparison.OrdinalIgnoreCase));
         }
 
-            await context.EditResponseAsync(GeneralStrings.VoicePlay.Replace("%station%", nowPlaying.Station.Name, StringComparison.OrdinalIgnoreCase));
+        [Command("queue"), Description("Shows the songs which will be played after this one.")]
+        public async ValueTask QueueAsync(SlashCommandContext context)
+        {
+            ArgumentNullException.ThrowIfNull(context, nameof(context));
+
+            _logger.CommandRequested(nameof(QueueAsync), context.User.GlobalName);
+
+            await context.DeferResponseAsync();
+
+            IEnumerable<ITrackQueueItem>? history = await _musicStreaming.HistoryAsync(context, true);
+            if (history?.Any() is not true)
+            {
+                await context.EditResponseAsync("There are no upcoming songs.");
+                return;
+            }
+
+            DiscordEmbed embed = EmbedBuilder.BuildMusicStreamingHistoryEmbed(history, true);
+            await context.EditResponseAsync(embed);
+        }
+
+        [Command("queue-clear"), Description("Clears the whole queue or only one song from it.")]
+        public async ValueTask QueueClearAsync
+        (
+            SlashCommandContext context,
+            [Description("The number of the song which you want to clear. Use the queue command first to get all song numbers.")] int songNumber = -1
+        )
+        {
+            ArgumentNullException.ThrowIfNull(context, nameof(context));
+
+            _logger.CommandRequested(nameof(QueueClearAsync), context.User.GlobalName);
+
+            await context.DeferResponseAsync();
+
+            if (!await _musicStreaming.ClearQueueAsync(context, songNumber))
+                return;
+
+            await context.EditResponseAsync((songNumber is -1) ? "The queue was cleared successfully." : $"I removed the song with the number **{songNumber}**.");
+        }
+
+        [Command("resume"), Description("Resume the paused player and play music again.")]
+        public async ValueTask ResumeAsync(SlashCommandContext context)
+        {
+            ArgumentNullException.ThrowIfNull(context, nameof(context));
+
+            _logger.CommandRequested(nameof(ResumeAsync), context.User.GlobalName);
+
+            await context.DeferResponseAsync();
+
+            if (await _musicStreaming.NowPlayingAsync(context) is null)
+            {
+                await context.EditResponseAsync("Nothing is playing right now.");
+                return;
+            }
+
+            await _musicStreaming.ResumeAsync(context);
+
+            await context.EditResponseAsync("The music has been resumed!");
+        }
+
+        [Command("skip"), Description("Skip the current played song and go over to the next one")]
+        public async ValueTask SkipAsync
+        (
+            SlashCommandContext context,
+            [Description("Specify how many songs you want to skip.")] int count = 1
+        )
+        {
+            ArgumentNullException.ThrowIfNull(context, nameof(context));
+
+            _logger.CommandRequested(nameof(SkipAsync), context.User.GlobalName);
+
+            await context.DeferResponseAsync();
+
+            if (await _musicStreaming.NowPlayingAsync(context) is null)
+            {
+                await context.EditResponseAsync("Nothing is playing right now.");
+                return;
+            }
+
+            await _musicStreaming.SkipSongAsync(context, count);
+
+            await context.EditResponseAsync($"I skipped **{count}** {((count is 1) ? "song" : "songs")}.");
         }
 
         [Command("stop"), Description("Stop the music.")]
