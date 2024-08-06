5c4b519bc41decd39809845b9ca025807a413cf7
Sellara <147769367+Sella-GH@users.noreply.github.com>
2024-08-06 22:57:05 +0200

    Fix possible null ref

--- src/AzzyBot.Bot/Utilities/EmbedBuilder.cs
+++ src/AzzyBot.Bot/Utilities/EmbedBuilder.cs
@@ -663,7 +663,7 @@ public static class EmbedBuilder
         };
 
         string songDuration = track.Duration.ToString(@"mm\:ss", CultureInfo.InvariantCulture);
-        string songElapsed = elapsed.Value.ToString(@"mm\:ss", CultureInfo.InvariantCulture);
+        string songElapsed = (elapsed.HasValue) ? elapsed.Value.ToString(@"mm\:ss", CultureInfo.InvariantCulture) : TimeSpan.FromTicks(0).ToString(@"mm\:ss", CultureInfo.InvariantCulture);
         string progressBar = Misc.GetProgressBar(14, elapsed.Value.TotalSeconds, track.Duration.TotalSeconds);
 
         fields.Add("Duration", new($"{progressBar} `[{songElapsed} / {songDuration}]`"));
