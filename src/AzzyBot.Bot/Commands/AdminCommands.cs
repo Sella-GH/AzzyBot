4d54b150af7b1d2ed5060b52efeee0bb5de50b84
Sella-GH <147769367+Sella-GH@users.noreply.github.com>
2024-08-18 18:16:54 +0200

    It would be better if you actually read what you do

--- src/AzzyBot.Bot/Commands/AdminCommands.cs
+++ src/AzzyBot.Bot/Commands/AdminCommands.cs
@@ -218,7 +218,7 @@ public sealed class AdminCommands
             if (string.IsNullOrWhiteSpace(logfile))
             {
                 dateTime = DateTime.Today.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
-                logfile = FileOperations.GetFilesInDirectory("Logs").First();
+                logfile = FileOperations.GetFilesInDirectory("Logs", true).First();
             }
             else
             {
