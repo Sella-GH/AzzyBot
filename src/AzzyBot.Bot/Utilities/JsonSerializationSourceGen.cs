db14ff59c9be96b82cb9ef9caa426ca91b1d0d31
Sella-GH <147769367+Sella-GH@users.noreply.github.com>
2024-12-15 19:35:00 +0100

    Fix SourceGen for AzuraCastFileService

--- src/AzzyBot.Bot/Utilities/JsonSerializationSourceGen.cs
+++ src/AzzyBot.Bot/Utilities/JsonSerializationSourceGen.cs
@@ -3,13 +3,14 @@ using AzzyBot.Bot.Utilities.Records;
 using AzzyBot.Bot.Utilities.Records.AzuraCast;
 using AzzyBot.Core.Utilities.Records;
 using System.Diagnostics.CodeAnalysis;
+using System.Collections.Generic;
 
 namespace AzzyBot.Bot.Utilities;
 
 [SuppressMessage("Roslynator", "RCS1251:Remove unnecessary braces from record declaration", Justification = "Class has to be empty.")]
 [JsonSourceGenerationOptions(WriteIndented = true, GenerationMode = JsonSourceGenerationMode.Serialization)]
 [JsonSerializable(typeof(AzuraAdminStationConfigRecord))]
-[JsonSerializable(typeof(AzuraFilesRecord))]
+[JsonSerializable(typeof(IEnumerable<AzuraFilesRecord>))]
 [JsonSerializable(typeof(AzuraFileUploadRecord))]
 [JsonSerializable(typeof(AzuraInternalRequestRecord))]
 [JsonSerializable(typeof(AzzyUpdateRecord))]
