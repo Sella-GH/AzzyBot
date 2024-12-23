99b889a4d52a00de361d16fa8dbc5821526b1337
Sella-GH <147769367+Sella-GH@users.noreply.github.com>
2024-12-23 20:26:44 +0100

    Optimize a little

--- src/AzzyBot.Core/Utilities/SoftwareStats.cs
+++ src/AzzyBot.Core/Utilities/SoftwareStats.cs
@@ -7,8 +7,10 @@ namespace AzzyBot.Core.Utilities;
 
 public static class SoftwareStats
 {
+    private static readonly string AppFilePath = $"{Path.Combine(AppContext.BaseDirectory, AppDomain.CurrentDomain.FriendlyName)}.dll";
+
     public static string GetAppAuthors
-        => FileVersionInfo.GetVersionInfo(GetAppFilePath).CompanyName ?? "Bot authors not found";
+        => FileVersionInfo.GetVersionInfo(AppFilePath).CompanyName ?? "Bot authors not found";
 
     public static string GetAppDotNetVersion
         => Environment.Version.ToString() ?? ".NET version not found";
@@ -17,10 +19,10 @@ public static class SoftwareStats
         => (GetAppName.EndsWith("Dev", StringComparison.OrdinalIgnoreCase)) ? Environments.Development : Environments.Production;
 
     public static string GetAppName
-        => FileVersionInfo.GetVersionInfo(GetAppFilePath).ProductName?.Split('.')[0] ?? "Bot name not found";
+        => FileVersionInfo.GetVersionInfo(AppFilePath).ProductName?.Split('.')[0] ?? "Bot name not found";
 
     public static string GetAppVersion
-        => FileVersionInfo.GetVersionInfo(GetAppFilePath).ProductVersion ?? "Bot version not found";
+        => FileVersionInfo.GetVersionInfo(AppFilePath).ProductVersion ?? "Bot version not found";
 
     public static double GetAppMemoryUsage()
     {
@@ -35,7 +37,4 @@ public static class SoftwareStats
 
         return app.StartTime;
     }
-
-    private static string GetAppFilePath
-        => $"{Path.Combine(AppContext.BaseDirectory, AppDomain.CurrentDomain.FriendlyName)}.dll";
 }
