15cd786f5d57278188698b0b5ce0dde5572755c2
Sella-GH <147769367+Sella-GH@users.noreply.github.com>
2024-12-23 18:19:24 +0100

    Add Exception Handling for `NCronJob`

--- src/AzzyBot.Bot/Services/CronJobManager.cs
+++ src/AzzyBot.Bot/Services/CronJobManager.cs
@@ -1,13 +1,24 @@
-﻿using AzzyBot.Bot.Services.CronJobs;
+﻿using System.Threading.Tasks;
+using System.Threading;
+using System;
+using AzzyBot.Bot.Services.CronJobs;
 using AzzyBot.Bot.Utilities.Records.AzuraCast;
 using NCronJob;
 
 namespace AzzyBot.Bot.Services;
 
-public sealed class CronJobManager(IInstantJobRegistry jobRegistry)
+public sealed class CronJobManager(IInstantJobRegistry jobRegistry, DiscordBotService botService) : IExceptionHandler
 {
     private readonly IInstantJobRegistry _jobRegistry = jobRegistry;
+    private readonly DiscordBotService _botService = botService;
 
     public void RunAzuraRequestJob(AzuraCustomQueueItemRecord record)
         => _jobRegistry.RunInstantJob<AzuraRequestJob>(record);
+
+    public async Task<bool> TryHandleAsync(IJobExecutionContext jobExecutionContext, Exception exception, CancellationToken cancellationToken)
+    {
+        await _botService.LogExceptionAsync(exception, DateTimeOffset.Now);
+
+        return true;
+    }
 }
