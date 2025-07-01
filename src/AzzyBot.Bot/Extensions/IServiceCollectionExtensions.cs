af4ac705ef8d9f651a8be9890b5a9a3b36caf431
Sella-GH <147769367+Sella-GH@users.noreply.github.com>
2025-07-01 21:15:40 +0200

    Create a separate job for AzuraCheckUpdates

--- src/AzzyBot.Bot/Extensions/IServiceCollectionExtensions.cs
+++ src/AzzyBot.Bot/Extensions/IServiceCollectionExtensions.cs
@@ -65,9 +65,9 @@ public static class IServiceCollectionExtensions
         {
             o.AddJob<AzuraCheckApiPermissionsJob>(j => j.WithCronExpression("0 */12 * * *").WithName(nameof(AzuraCheckApiPermissionsJob))); // Every 12 hours
             o.AddJob<AzuraCheckFileChangesJob>(j => j.WithCronExpression("0 */1 * * *").WithName(nameof(AzuraCheckFileChangesJob))); // Every hour
+            o.AddJob<AzuraCheckUpdatesJob>(j => j.WithCronExpression("0 */6 * * *").WithName(nameof(AzuraCheckUpdatesJob))); // Every 6 hours
             o.AddJob<AzuraRequestJob>();
             o.AddJob<AzuraStatusPingJob>(j => j.WithCronExpression("*/15 * * * *").WithName(nameof(AzuraStatusPingJob))); // Every 15 minutes
-            o.AddJob<AzzyBotGlobalChecksJob>(j => j.WithCronExpression("*/15 * * * *").WithName(nameof(AzzyBotGlobalChecksJob))); // Every 15 minutes
             o.AddJob<AzzyBotCheckPermissionsJob>(j => j.WithCronExpression("0 0 */12 * *").WithName(nameof(AzzyBotCheckPermissionsJob))); // Every 12 hours
             o.AddJob<AzzyBotUpdateCheckJob>(j => j.WithCronExpression("0 */6 * * *").WithName(nameof(AzzyBotUpdateCheckJob))); // Every 6 hours
             o.AddJob<DatabaseCleaningJob>(j => j.WithCronExpression("0 0 */1 * *").WithName(nameof(DatabaseCleaningJob))); // Every day
---
+++ src/AzzyBot.Bot/Services/CronJobs/AzuraCheckUpdatesJob.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using AzzyBot.Bot.Services.Modules;
+using AzzyBot.Data.Entities;
+using AzzyBot.Data.Services;
+
+using NCronJob;
+
+namespace AzzyBot.Bot.Services.CronJobs;
+
+public sealed class AzuraCheckUpdatesJob(AzuraCastUpdateService azuraUpdateService, DbActions dbActions, DiscordBotService botService) : IJob
+{
+    private readonly AzuraCastUpdateService _azuraUpdateService = azuraUpdateService;
+    private readonly DbActions _dbActions = dbActions;
+    private readonly DiscordBotService _botService = botService;
+
+    public async Task RunAsync(IJobExecutionContext context, CancellationToken token)
+    {
+        try
+        {
+            IReadOnlyList<GuildEntity> guilds = await _dbActions.GetGuildsAsync(loadEverything: true);
+            if (!guilds.Any())
+                return;
+
+            foreach (GuildEntity guild in guilds.Where(g => g.AzuraCast?.IsOnline is true && g.AzuraCast.Checks.Updates))
+            {
+                await _azuraUpdateService.CheckForAzuraCastUpdatesAsync(guild.AzuraCast!);
+            }
+        }
+        catch (Exception ex) when (ex is not OperationCanceledException or TaskCanceledException)
+        {
+            await _botService.LogExceptionAsync(ex, DateTimeOffset.Now);
+        }
+    }
+}
