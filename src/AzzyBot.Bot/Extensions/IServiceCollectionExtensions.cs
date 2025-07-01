b0eb80799db27d6b7a8fb145e9a053bae6a7b236
Sella-GH <147769367+Sella-GH@users.noreply.github.com>
2025-07-01 20:52:46 +0200

    Fix the cron for AzuraStatusPingJob

--- src/AzzyBot.Bot/Extensions/IServiceCollectionExtensions.cs
+++ src/AzzyBot.Bot/Extensions/IServiceCollectionExtensions.cs
@@ -65,7 +65,7 @@ public static class IServiceCollectionExtensions
         {
             o.AddJob<AzuraCheckApiPermissionsJob>(j => j.WithCronExpression("0 */12 * * *").WithName(nameof(AzuraCheckApiPermissionsJob))); // Every 12 hours
             o.AddJob<AzuraRequestJob>();
-            o.AddJob<AzuraStatusPingJob>(j => j.WithCronExpression("0 */15 * * * *").WithName(nameof(AzuraStatusPingJob))); // Every 15 minutes
+            o.AddJob<AzuraStatusPingJob>(j => j.WithCronExpression("*/15 * * * *").WithName(nameof(AzuraStatusPingJob))); // Every 15 minutes
             o.AddJob<AzzyBotGlobalChecksJob>(j => j.WithCronExpression("*/15 * * * *").WithName(nameof(AzzyBotGlobalChecksJob))); // Every 15 minutes
             o.AddJob<AzzyBotCheckPermissionsJob>(j => j.WithCronExpression("0 0 */12 * *").WithName(nameof(AzzyBotCheckPermissionsJob))); // Every 12 hours
             o.AddJob<AzzyBotUpdateCheckJob>(j => j.WithCronExpression("0 */6 * * *").WithName(nameof(AzzyBotUpdateCheckJob))); // Every 6 hours
