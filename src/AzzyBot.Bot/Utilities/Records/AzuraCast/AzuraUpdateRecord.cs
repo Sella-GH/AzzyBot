65c9609e09005a67ccb731b09cb603f5c276c7d3
Sella-GH <147769367+Sella-GH@users.noreply.github.com>
2025-07-18 22:52:58 +0200

    Rework the AzuraCastUpdateRecord to be consistent with the current output (#365)

--- src/AzzyBot.Bot/Utilities/Records/AzuraCast/AzuraUpdateRecord.cs
+++ src/AzzyBot.Bot/Utilities/Records/AzuraCast/AzuraUpdateRecord.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Text.Json.Serialization;
+﻿using System.Text.Json.Serialization;
 
 namespace AzzyBot.Bot.Utilities.Records.AzuraCast;
 
@@ -12,35 +11,35 @@ public sealed record AzuraUpdateRecord
     /// The current release version of AzuraCast.
     /// </summary>
     [JsonPropertyName("current_release")]
-    public required string CurrentRelease { get; init; }
+    public string? CurrentRelease { get; init; }
 
     /// <summary>
     /// The latest release version of AzuraCast.
     /// </summary>
     [JsonPropertyName("latest_release")]
-    public required string LatestRelease { get; init; }
+    public string? LatestRelease { get; init; }
 
     /// <summary>
     /// Whether the current release is rolling release and needs updates.
     /// </summary>
     [JsonPropertyName("needs_rolling_update")]
-    public required bool NeedsRollingUpdate { get; init; }
-
-    /// <summary>
-    /// The list of updates available.
-    /// </summary>
-    [JsonPropertyName("rolling_updates_list")]
-    public IReadOnlyList<string> RollingUpdatesList { get; init; } = [];
+    public bool NeedsRollingUpdate { get; init; }
 
     /// <summary>
     /// Whether the current release is stable release and needs updates.
     /// </summary>
     [JsonPropertyName("needs_release_update")]
-    public required bool NeedsReleaseUpdate { get; init; }
+    public bool NeedsReleaseUpdate { get; init; }
+
+    /// <summary>
+    /// The number of rolling updates available for the current release.
+    /// </summary>
+    [JsonPropertyName("rolling_updates_available")]
+    public int RollingUpdatesAvailable { get; init; }
 
     /// <summary>
     /// Whether the current rolling release can be switched to a stable release.
     /// </summary>
     [JsonPropertyName("can_switch_to_stable")]
-    public required bool CanSwitchToStable { get; init; }
+    public bool CanSwitchToStable { get; init; }
 }
