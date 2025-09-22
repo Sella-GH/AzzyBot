c6df330721d85a9ea1771deeb477ac37bc172ae8
Sella-GH <147769367+Sella-GH@users.noreply.github.com>
2025-09-22 08:11:19 +0200

    Add new migrations

---
+++ src/AzzyBot.Data/Migrations/20250922061106_UnusedGuildsRemoval.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AzzyBot.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UnusedGuildsRemoval : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "RemindersStart",
+                table: "Guilds",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastGuildReminderCheck",
+                table: "AzzyBot",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RemindersStart",
+                table: "Guilds");
+
+            migrationBuilder.DropColumn(
+                name: "LastGuildReminderCheck",
+                table: "AzzyBot");
+        }
+    }
+}
