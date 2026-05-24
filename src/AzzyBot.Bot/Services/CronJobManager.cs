5a08c5b3752794f3e25666c5569ce9ba28da7698
Sella-GH <147769367+Sella-GH@users.noreply.github.com>
2026-05-24 22:01:00 +0200

    Extend CronJobManager

--- src/AzzyBot.Bot/Services/CronJobManager.cs
+++ src/AzzyBot.Bot/Services/CronJobManager.cs
@@ -15,11 +15,41 @@ public sealed class CronJobManager(IInstantJobRegistry jobRegistry, IDiscordBotS
     private readonly IInstantJobRegistry _jobRegistry = jobRegistry;
     private readonly IDiscordBotService _botService = botService;
 
-    public void RunAzzyBotInactiveGuildJob()
-        => _jobRegistry.RunInstantJob<AzzyBotInactiveGuildJob>();
+    public void RunAzuraCheckApiPermissionsJob()
+        => _jobRegistry.RunInstantJob<AzuraCheckApiPermissionsJob>();
+
+    public void RunAzuraCheckFileChangesJob()
+        => _jobRegistry.RunInstantJob<AzuraCheckFileChangesJob>();
+
+    public void RunAzuraCheckUpdatesJob()
+        => _jobRegistry.RunInstantJob<AzuraCheckUpdatesJob>();
+
+    public void RunAzuraPersistentNowPlayingJob()
+        => _jobRegistry.RunInstantJob<AzuraPersistentNowPlayingJob>();
 
     public void RunAzuraRequestJob(AzuraCustomQueueItemModel queueItem)
         => _jobRegistry.RunInstantJob<AzuraRequestJob>(queueItem);
+
+    public void RunAzuraStatusPingJob()
+        => _jobRegistry.RunInstantJob<AzuraStatusPingJob>();
+
+    public void RunAzzyBotCheckPermissionsJob()
+        => _jobRegistry.RunInstantJob<AzzyBotCheckPermissionsJob>();
+
+    public void RunAzzyBotInactiveGuildJob()
+        => _jobRegistry.RunInstantJob<AzzyBotInactiveGuildJob>();
+
+    public void RunAzzyBotUpdateCheckJob()
+        => _jobRegistry.RunInstantJob<AzzyBotUpdateCheckJob>();
+
+    public void RunDatabaseCleaningJob()
+        => _jobRegistry.RunInstantJob<DatabaseCleaningJob>();
+
+    public void RunLogfileCleaningJob()
+        => _jobRegistry.RunInstantJob<LogfileCleaningJob>();
+
+    public void RunMusicStreamingPersistentNowPlayingJob()
+        => _jobRegistry.RunInstantJob<MusicStreamingPersistentNowPlayingJob>();
 
     public async Task<bool> TryHandleAsync(IJobExecutionContext jobExecutionContext, Exception exception, CancellationToken cancellationToken)
     {
--- src/AzzyBot.Bot/Services/Interfaces/ICronJobManager.cs
+++ src/AzzyBot.Bot/Services/Interfaces/ICronJobManager.cs
@@ -6,6 +6,16 @@ namespace AzzyBot.Bot.Services.Interfaces;
 
 public interface ICronJobManager : IExceptionHandler
 {
-    void RunAzzyBotInactiveGuildJob();
+    void RunAzuraCheckApiPermissionsJob();
+    void RunAzuraCheckFileChangesJob();
+    void RunAzuraCheckUpdatesJob();
+    void RunAzuraPersistentNowPlayingJob();
     void RunAzuraRequestJob(AzuraCustomQueueItemModel queueItem);
+    void RunAzuraStatusPingJob();
+    void RunAzzyBotCheckPermissionsJob();
+    void RunAzzyBotInactiveGuildJob();
+    void RunAzzyBotUpdateCheckJob();
+    void RunDatabaseCleaningJob();
+    void RunLogfileCleaningJob();
+    void RunMusicStreamingPersistentNowPlayingJob();
 }
